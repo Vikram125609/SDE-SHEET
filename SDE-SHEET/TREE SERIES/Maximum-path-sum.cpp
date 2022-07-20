705c0e5ff6ea6fc075c327f22e8e94d4516b94ff
Vikram Singh <vs125609@gmail.com>
2022-07-20 19:15:09 +0530

    Striver Maximum Path Sum Solution

--- SDE-SHEET/TREE SERIES/Maximum-path-sum.cpp
+++ SDE-SHEET/TREE SERIES/Maximum-path-sum.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello World" << endl;
-    return 0;
-}
+class Solution {
+public:
+    int maxPathSum(TreeNode*root,int &maxi)
+    {
+        if(root == NULL)
+        {
+            return 0;
+        }
+        int leftSum = max(0,maxPathSum(root->left,maxi));
+        int rightSum = max(0,maxPathSum(root->right,maxi));
+        maxi = max(maxi,leftSum + rightSum + root->val);
+        return max(leftSum,rightSum) + root->val;
+    }
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        maxPathSum(root,maxi);
+        return maxi;
+    }
+};
